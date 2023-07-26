b9ef8bb4e840520b20fbd330c0419d05b681aa23
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-07-26 14:32:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,7 @@
-print("oi")
+from flask import Flask
+app = Flask('hello-world')
+@app.route('/')
+def hello():
+ return "CloudageSkill Microservices\n"
+if __name__ == '__main__':
+ app.run(host = '0.0.0.0', port = 8080)
