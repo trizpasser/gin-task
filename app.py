381f69ff0ff0071d76071754fa42bc2875f3936d
Juliana Marques <115050272+juliana-marques@users.noreply.github.com>
2023-07-26 14:40:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "CloudageSkill Microservices\n"
+ return "Teste\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
